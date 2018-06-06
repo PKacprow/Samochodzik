d8208af11e916c46a64c95d785bc9c004ceca59e
Patryk Cieślak <187451@edu.p.lodz.pl>
2018-06-07 00:02:56 +0200

    Added connection after click on relevant device name from list

--- MyApplicationAutko2/app/src/main/java/com/example/piotrek/myapplicationautko2/Devices.java
+++ MyApplicationAutko2/app/src/main/java/com/example/piotrek/myapplicationautko2/Devices.java
@@ -1,16 +1,25 @@
 package com.example.piotrek.myapplicationautko2;
 
+import android.app.ProgressDialog;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Set;
+import java.util.UUID;
 
 public class Devices extends AppCompatActivity
 {
@@ -52,9 +61,43 @@ public class Devices extends AppCompatActivity
         btnPaired.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //ToDO Add action after click on device name
+                pairedDevicesList();
             }
         });
-
     }
+
+        private void pairedDevicesList() {
+            pairedDevices = myBluetooth.getBondedDevices();
+            ArrayList list = new ArrayList();
+
+            if (pairedDevices.size() > 0) {
+                for (BluetoothDevice bt : pairedDevices) {
+                    list.add(bt.getName() + "\n" + bt.getAddress()); //Get the device's name and the address
+                }
+            } else {
+                Toast.makeText(getApplicationContext(), "No Paired Bluetooth Devices Found.", Toast.LENGTH_LONG).show();
+            }
+
+            final ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, list);
+            devicelist.setAdapter(adapter);
+            devicelist.setOnItemClickListener(myListClickListener); //Method called when the device from the list is clicked
+
+        }
+
+
+        private AdapterView.OnItemClickListener myListClickListener = new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> av, View v, int arg2, long arg3) {
+                // Get the device MAC address, the last 17 chars in the View
+                String info = ((TextView) v).getText().toString();
+                String address = info.substring(info.length() - 17);
+
+                // Make an intent to start next activity.
+                Intent i = new Intent(Devices.this, MainActivity.class);
+
+                //Change the activity.
+                i.putExtra(EXTRA_ADDRESS, address); //this will be received at ledControl (class) Activity
+                startActivity(i);
+            }
+        };
+
 }
