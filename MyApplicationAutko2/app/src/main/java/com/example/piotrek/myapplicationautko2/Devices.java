d2ccf1fe3d267659a53e96a309f72928e4c23e8c
ajzo-2 <ajzo-2@sr.dmcs.pl>
2018-06-06 23:11:25 +0200

    Added Devices.java class.

---
+++ MyApplicationAutko2/app/src/main/java/com/example/piotrek/myapplicationautko2/Devices.java
@@ -0,0 +1,60 @@
+package com.example.piotrek.myapplicationautko2;
+
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.Set;
+
+public class Devices extends AppCompatActivity
+{
+    //widgets
+    Button btnPaired;
+    ListView devicelist;
+    //Bluetooth
+    private BluetoothAdapter myBluetooth = null;
+    private Set<BluetoothDevice> pairedDevices;
+    public static String EXTRA_ADDRESS = "device_address";
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_devices);
+
+        //Calling widgets
+        btnPaired = (Button) findViewById(R.id.button);
+        devicelist = (ListView) findViewById(R.id.listView);
+
+        //if the device has bluetooth
+        myBluetooth = BluetoothAdapter.getDefaultAdapter();
+
+        if (myBluetooth == null) {
+            //Show a message when no bluetooth adapter
+            Toast.makeText(getApplicationContext(), "Bluetooth Device Not Available", Toast.LENGTH_LONG).show();
+
+            //finish apk
+            finish();
+        } else if (!myBluetooth.isEnabled()) {
+            //Ask to the user turn the bluetooth on
+            Intent turnBTon = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(turnBTon, 1);
+        }
+
+        btnPaired.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //ToDO Add action after click on device name
+            }
+        });
+
+    }
+}
