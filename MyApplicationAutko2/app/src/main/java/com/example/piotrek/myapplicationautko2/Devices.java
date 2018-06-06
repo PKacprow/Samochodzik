6172064dd1535a57f65b760dbd4917da88bc286f
Patryk Cieślak <187451@edu.p.lodz.pl>
2018-06-07 00:09:06 +0200

    Added connection after click on relevant device name from list

--- MyApplicationAutko2/app/src/main/java/com/example/piotrek/myapplicationautko2/Devices.java
+++ MyApplicationAutko2/app/src/main/java/com/example/piotrek/myapplicationautko2/Devices.java
@@ -64,40 +64,40 @@ public class Devices extends AppCompatActivity
                 pairedDevicesList();
             }
         });
+
     }
 
-        private void pairedDevicesList() {
-            pairedDevices = myBluetooth.getBondedDevices();
-            ArrayList list = new ArrayList();
+    private void pairedDevicesList() {
+        pairedDevices = myBluetooth.getBondedDevices();
+        ArrayList list = new ArrayList();
 
-            if (pairedDevices.size() > 0) {
-                for (BluetoothDevice bt : pairedDevices) {
-                    list.add(bt.getName() + "\n" + bt.getAddress()); //Get the device's name and the address
-                }
-            } else {
-                Toast.makeText(getApplicationContext(), "No Paired Bluetooth Devices Found.", Toast.LENGTH_LONG).show();
+        if (pairedDevices.size() > 0) {
+            for (BluetoothDevice bt : pairedDevices) {
+                list.add(bt.getName() + "\n" + bt.getAddress()); //Get the device's name and the address
             }
-
-            final ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, list);
-            devicelist.setAdapter(adapter);
-            devicelist.setOnItemClickListener(myListClickListener); //Method called when the device from the list is clicked
-
+        } else {
+            Toast.makeText(getApplicationContext(), "No Paired Bluetooth Devices Found.", Toast.LENGTH_LONG).show();
         }
 
+        final ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, list);
+        devicelist.setAdapter(adapter);
+        devicelist.setOnItemClickListener(myListClickListener); //Method called when the device from the list is clicked
 
-        private AdapterView.OnItemClickListener myListClickListener = new AdapterView.OnItemClickListener() {
-            public void onItemClick(AdapterView<?> av, View v, int arg2, long arg3) {
-                // Get the device MAC address, the last 17 chars in the View
-                String info = ((TextView) v).getText().toString();
-                String address = info.substring(info.length() - 17);
+    }
 
-                // Make an intent to start next activity.
-                Intent i = new Intent(Devices.this, MainActivity.class);
 
-                //Change the activity.
-                i.putExtra(EXTRA_ADDRESS, address); //this will be received at ledControl (class) Activity
-                startActivity(i);
-            }
-        };
+    private AdapterView.OnItemClickListener myListClickListener = new AdapterView.OnItemClickListener() {
+        public void onItemClick(AdapterView<?> av, View v, int arg2, long arg3) {
+            // Get the device MAC address, the last 17 chars in the View
+            String info = ((TextView) v).getText().toString();
+            String address = info.substring(info.length() - 17);
 
+            // Make an intent to start next activity.
+            Intent i = new Intent(Devices.this, MainActivity.class);
+
+            //Change the activity.
+            i.putExtra(EXTRA_ADDRESS, address); //this will be received at ledControl (class) Activity
+            startActivity(i);
+        }
+    };
 }
